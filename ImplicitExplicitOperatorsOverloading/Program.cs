3641b660d00dfbded2bd8f658bb7b0f84818855c
volkan.onder <volkan.onder@arksoft.com.tr>
2024-03-28 17:53:26 +0300

    Add project files.

---
+++ ImplicitExplicitOperatorsOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿//Error Scenarios
+
+A a1 = new B();
+A a2 = (A) new B();
+
+B b1 = (B)new A();
+
+class A
+{
+    public static explicit operator B(A a)
+    {
+        return new B();
+    }
+}
+
+class B
+{
+    public static implicit operator A(B b)
+    {
+        return new A();
+    }
+}
