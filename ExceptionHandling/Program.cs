a76ee7c96968b5a3c5998c55e3d1f7adc27d095a
vlkan <thkvolkanonder@gmail.com>
2024-04-07 17:20:11 +0300

    exception handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿//try
+//{
+//    int a = 1, b = 2;
+//    Console.WriteLine(a / b);
+//}
+//catch (Exception ex)
+//{
+
+//}
+try
+{
+    while (true)
+    {
+        var key = Console.ReadKey();
+
+        if (key.Key == ConsoleKey.R)
+        {
+            throw new CustomException("Pressed To R") 
+            {
+                ExceptionGuid = Guid.NewGuid(),
+            };
+        }
+        else
+        {
+            Console.WriteLine(key.Key);
+        }
+    }
+}
+catch (Exception ex)
+{
+    throw;
+}
+
+
+class CustomException : Exception
+{
+    public Guid ExceptionGuid { get; set; }
+    public CustomException() : base("Custom Hata")
+    {
+        
+    }
+
+    public CustomException(string message) : base(message)
+    {
+        
+    }
+}
