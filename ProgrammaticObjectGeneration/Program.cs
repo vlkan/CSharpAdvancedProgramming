a34a62d0736ac02e69600afe83407b74fa950656
vlkan <thkvolkanonder@gmail.com>
2024-04-08 14:56:18 +0300

    programatic - dynamic object creation

---
+++ ProgrammaticObjectGeneration/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Dynamic;
+
+#region CreateWithActivator
+//MyClass m = (MyClass)Activator.CreateInstance(typeof(MyClass));
+#endregion
+
+#region CreateWithDynamicObjectClass
+
+//dynamic my = new MyClass();
+//my.DynamicProp1 = 3;
+//my.DynamicProp2 = "4";
+
+//Console.WriteLine(my.DynamicProp1);
+#endregion
+
+#region CreateWithDynamicKeyword - ExpandoObject
+
+dynamic obj = new ExpandoObject();
+obj.P1 = 123;
+obj.P2 = "1234";
+
+Console.WriteLine(obj.P1);
+#endregion
+
+class MyClass : DynamicObject
+{
+    public MyClass()
+    {
+        Console.WriteLine($"{nameof(MyClass)} instance created.");
+    }
+
+    private readonly Dictionary<string, object> _properties = new();
+
+    public override bool TrySetMember(SetMemberBinder binder, object? value)
+    {
+        _properties.Add(binder.Name, value);
+        return true;
+    }
+
+    public override bool TryGetMember(GetMemberBinder binder, out object? result)
+    {
+        return _properties.TryGetValue(binder.Name, out result);
+    }
+}
