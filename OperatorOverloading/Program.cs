b2d03ab15dd695350d48369a6e56d1c043b030ad
vlkan <thkvolkanonder@gmail.com>
2024-07-07 16:39:29 +0300

    Operator Overloading

---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student student = new()
+        {
+            Name = "Test",
+        };
+
+        Book book = new()
+        {
+            Name = "Foundation",
+            Author = "Isaac Asimov"
+        };
+
+        //student + book; //Error (First Attempt)
+
+        var result = student + book;
+    }
+}
+
+class Book()
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+
+    public static Student operator +(Student student, Book book)
+    {
+        student.Books.Add(book);
+        return student;
+    }
+}
+
+class Student()
+{
+    public string Name { get; set; }
+    public List<Book> Books { get; set; } = new();
+}
