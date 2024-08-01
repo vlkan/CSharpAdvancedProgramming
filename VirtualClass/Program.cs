35b716db99f18547dc95a8866d8a74821a55f3bf
volkan.onder <volkan.onder@arksoft.com.tr>
2024-08-01 14:57:17 +0300

    virtual methods

---
+++ VirtualClass/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var aService = new AService();
+        var bService = new BService();
+
+        aService.OnStart("Volkan");
+        bService.OnStart("Volkan");
+    }
+}
+
+public class BaseClass
+{
+    public virtual string OnStart(string message)
+    {
+        Console.Write("Hello: " + message);
+        return message;
+    }
+}
+
+public class AService : BaseClass
+{
+    public override string OnStart(string message)
+    {
+        Console.Write(message);
+        return message;
+    }
+}
+
+public class BService : BaseClass 
+{
+    
+}
